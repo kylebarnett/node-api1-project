09de1b4263ecb52c7d0b28befcfbc69428d4a9fb
Kyle Barnett <kbarnett23@gmail.com>
2019-10-29 15:48:34 -0600

    get users working

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require('express'); //importing the express library similar to the FE method of doing it
+const db = require('./data/db'); //importing the necessary database file
+
+const server = express(); //creates the server
+const port = 8000; // creating a port variable to listen for
+
+server.get('/api/users', (req, res) => {
+  db.find().then(users => {
+    res.json(users)
+  }).catch(err => {
+    res.json({error: "Users information could not be retrieved."})
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+
