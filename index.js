e9e2ec40a146d056ace8f5a26ec50f9d02734560
Kyle Barnett <kbarnett23@gmail.com>
2019-10-29 16:10:15 -0600

    post is working

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const express = require('express'); //importing the express library similar to t
 const db = require('./data/db'); //importing the necessary database file
 
 const server = express(); //creates the server
+
+//middleware
+server.use(express.json()); //teaches express how to read JSON--needed for post and put to work
 const port = 8000; // creating a port variable to listen for
 
 server.get('/api/users', (req, res) => {
   db.find().then(users => {
     res.json(users)
   }).catch(err => {
-    res.json({error: "Users information could not be retrieved."})
+    res.status(500).json({ error: "Users information could not be retrieved." })
+  })
+})
+
+server.post('/api/users', (req, res) => {
+  const userInfo = req.body;
+  console.log("test", userInfo)
+  db.insert(userInfo).then(user => {
+    res.json(user)
+  }).catch(err => {
+    res.status(500).json({ error: "User information could not be posted." })
   })
 })
 
